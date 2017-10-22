7d96147b2f6e2e74cf3be2b95b915855823a03dc
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-22 19:46:26 +0100

    Data binding in a component
    Displaying event details using EventsListComponent

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -5,9 +5,33 @@ import { Component } from '@angular/core'
     template: `
         <div>
             <h1>Upcoming Angular 2 events</h1>
+            <hr>
+            <div class="well hoverwell thumbnail">
+                <h2>{{event.name}}</h2>
+                <div>Date: {{event.date}}</div>
+                <div>Time: {{event.time}}</div>
+                <div>Price: \${{event.price}} </div>
+                <div>
+                    <span>Location: {{event.location.address}}</span>
+                    <span>&nbsp;</span>
+                    <span>{{event.location.city}},{{event.location.country}}</span>
+                </div>
+            </div>
         </div>
     `
 })
 export class EventsListComponent{
-
+    event = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '9/26/2036',
+        time: '10:00 am',
+        price: 599.99,
+        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        location: {
+          address: '1057 DT',
+          city: 'London',
+          country: 'England'
+        },
+    }
 }
