3c625f5cb42f3cb033ff54769c3d952e6b45d914
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-22 19:21:41 +0100

    Root component uses another component
    Root component, EventsAppComponent,  uses EventsListComponent

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'events-app',
-    template: '<h2>Hello World </h2>'
+    template: '<events-list></events-list>'
 })
 export class EventsAppComponent {
 
---
+++ app/events/events-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'events-list',
+    template: `
+        <div>
+            <h1>Upcoming Angular 2 events</h1>
+        </div>
+    `
+})
+export class EventsListComponent{
+
+}
