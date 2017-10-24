5f3c767a61b3b130b44fbc8dd322c44055eb3c92
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-24 23:37:37 +0100

    Communicating with child component
    Passing “event1” object from EventsListComponent to “event” object of
    EventThumbnailComponent through “[event]” attribute of
    “<event-thumbnail>” in EventsListComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
+import { EventThumbnailComponent } from './events/event-thumbnail.component'
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [
         EventsAppComponent, 
-        EventsListComponent
+        EventsListComponent,
+        EventThumbnailComponent
     ],
     bootstrap: [EventsAppComponent]
 
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'events-list',
-    templateUrl: 'app/events/events-list.component.html'
+    template: `
+    <div>
+        <h1>Upcoming Angular 2 events</h1>
+        <hr>    
+        <event-thumbnail [event] = event1 > </event-thumbnail>
+    </div>
+    `
 })
 export class EventsListComponent{
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '9/26/2036',
