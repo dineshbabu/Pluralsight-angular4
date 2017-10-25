305f07e28f89e215ab365c014b9b8c3e07c692b2
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-25 23:44:47 +0100

    Adding a site header at the top

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser'
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import { NavBarComponent } from './nav/nav-bar.component'
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [
         EventsAppComponent, 
         EventsListComponent,
-        EventThumbnailComponent
+        EventThumbnailComponent,
+        NavBarComponent
     ],
     bootstrap: [EventsAppComponent]
 
---
+++ app/nav/nav-bar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'nav-bar',
+    templateUrl: '/app/nav/navbar.component.html'
+})
+export class NavBarComponent{
+
+}
