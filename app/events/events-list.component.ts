d4db21c01a901f7bf09088d4ddd8d05494094811
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-25 23:29:22 +0100

    Removing stuff not necessary for next section

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -5,16 +5,10 @@ import { Component } from '@angular/core'
     template: `
     <div>
         <h1>Upcoming Angular 2 events</h1>
-        <hr>    
-        <div class="well">
-            <div>Hello World</div>
-        </div>
+        <hr> 
         <event-thumbnail [event] = event1 > </event-thumbnail>
     </div>
-    `,
-    styles: [`
-    .well div { color: red; }
-    `]
+    `
 })
 export class EventsListComponent{
     event1 = {
