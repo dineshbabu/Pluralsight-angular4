f47aa9c7ba977d845850b70faca7582b5f866012
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-25 23:21:36 +0100

    Removed code not necessary for next section

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -22,8 +22,4 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 })
 export class EventThumbnailComponent{
     @Input() event: any
-    childVariable: any = 'Child variable value'
-    logFoo(){
-        console.log('Child component logged foo')
-    }
 }
