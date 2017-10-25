54aa0638e6fe41b81e8e21f62671929c04c46610
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-25 23:27:28 +0100

    Applying styles to only to desired component
    <div> with “well” class is available in both parent and child
    component.But red colour which is part of “well” style in parent gets
    applied only to parent div and not child div

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -6,10 +6,15 @@ import { Component } from '@angular/core'
     <div>
         <h1>Upcoming Angular 2 events</h1>
         <hr>    
-        <event-thumbnail #thumbnail [event] = event1 > </event-thumbnail>
-        <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log foo from child</button>
+        <div class="well">
+            <div>Hello World</div>
+        </div>
+        <event-thumbnail [event] = event1 > </event-thumbnail>
     </div>
-    `
+    `,
+    styles: [`
+    .well div { color: red; }
+    `]
 })
 export class EventsListComponent{
     event1 = {
