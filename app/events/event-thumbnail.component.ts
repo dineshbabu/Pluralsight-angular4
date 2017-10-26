12854ffc34e395b332f5b1cf2b7f8e67bc326b35
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-26 23:30:55 +0100

    Class binding to style elements
    [class.green] will apply .green class style

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>Date: {{event?.date}}</div>
-    <div [ngSwitch]="event?.time">
+    <div [class.green]="event?.time=='8:00 am'" [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late start)</span>
@@ -24,6 +24,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     `
     ,
     styles: [`
+    .green { color: #003300 !important;}
     .thumbnail {min-height: 210px;}
     .pad-left { margin-left: 10px;}
     .well div { color: #bbb; }
