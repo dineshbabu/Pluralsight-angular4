12f150954a20129e7df8dcfccab8b4b0e19a7d7f
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-26 23:40:58 +0100

    [ngClass]: To apply mutilple css classes
    Getting styles from a method which returns string of class names

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -35,7 +35,9 @@ export class EventThumbnailComponent{
     @Input() event: any
 
     getStartTimeClass(){
-        const isEarlyStart = this.event&&this.event.time==='8:00 am'
-        return {green: isEarlyStart, bold: isEarlyStart}
+        if(this.event&&this.event.time==='8:00 am'){
+            return 'green bold'
+        }
+        return ''
     }
 }
