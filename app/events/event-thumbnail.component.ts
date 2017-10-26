b52b5b6ea9bddf7a80b5199424be2d6075053723
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-26 23:35:53 +0100

    [ngClass]: To apply mutilple css classes
    Apply bold and green class

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>Date: {{event?.date}}</div>
-    <div [class.green]="event?.time=='8:00 am'" [ngSwitch]="event?.time">
+    <div [ngClass]="{green: event?.time=='8:00 am', bold: event?.time=='8:00 am'}" [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late start)</span>
@@ -25,6 +25,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     ,
     styles: [`
     .green { color: #003300 !important;}
+    .bold { font-weight: bold;}
     .thumbnail {min-height: 210px;}
     .pad-left { margin-left: 10px;}
     .well div { color: #bbb; }
