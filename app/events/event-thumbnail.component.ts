f8bff78e15578c706ee3cee0f797b4cc5836883c
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-26 22:55:42 +0100

    Hiding elements conditionally using *ngIf structural directive

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -8,11 +8,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div>Date: {{event?.date}}</div>
     <div>Time: {{event?.time}}</div>
     <div>Price: \${{event?.price}} </div>
-    <div>
+    <div *ngIf="event?.location">
         <span>Location: {{event?.location?.address}}</span>
         <span class="pad-left">{{event?.location?.city}},{{event?.location?.country}}</span>
     </div>
-    <div>
+    <div *ngIf="event?.onlineUrl">
         Online : {{event?.onlineUrl}}
     </div>
 </div>
