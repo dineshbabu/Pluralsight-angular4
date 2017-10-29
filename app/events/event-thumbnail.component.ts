10b4f7cf567debc378c7147fde40440a1e203a7a
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-29 14:49:52 +0000

    Style: Applying single style using "[style.xxx]"

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>Date: {{event?.date}}</div>
-    <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+    <div [style.color]="event.time === '8:00 am' ? 'green': 'black'" [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late start)</span>
