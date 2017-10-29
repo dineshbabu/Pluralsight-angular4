a67a9dabf54ff0be3b2f2aa4ec5ac99e04e69542
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-29 17:18:16 +0000

    Services: Creating a service
    1. Create EventService which will have events data and
    2. Inject Eventservice to EventsListComponent’s constructor
    3. In EventsListComponent, get events data from the eventsService in
    ngOnInit method

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,6 +6,8 @@ import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/nav-bar.component'
 
+import { EventService } from './events/shared/event-service.component' 
+
 @NgModule({
     imports: [BrowserModule],
     declarations: [
@@ -14,6 +16,7 @@ import { NavBarComponent } from './nav/nav-bar.component'
         EventThumbnailComponent,
         NavBarComponent
     ],
+    providers: [EventService],
     bootstrap: [EventsAppComponent]
 
 })
