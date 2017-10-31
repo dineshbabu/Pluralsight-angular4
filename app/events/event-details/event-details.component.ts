3fcb5f6f51f9dd5826e701d4155c03d3bcef58f9
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-10-31 22:55:41 +0000

    Router; Getting parameter value from the URL path
    Removing the hard coding of value of event id and replacing it with
    route parameter value

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from '../shared/event-service'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
     templateUrl : '/app/events/event-details/event-details.component.html',
@@ -11,11 +12,11 @@ import { EventService } from '../shared/event-service'
 export class EventDetailsComponent implements OnInit{
     event: any
 
-    constructor(private eventService: EventService){
+    constructor(private eventService: EventService, private route: ActivatedRoute){
 
     }
 
     ngOnInit(){
-        this.event = this.eventService.getEvent(1)
+        this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
     }
 }
