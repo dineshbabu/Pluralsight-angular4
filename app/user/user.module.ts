55338eff16801c7256bc0372a6a3144b1dffc324
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-11-06 21:19:17 +0000

    Lazily loading a feature module
    1. User module is the feature module for user related stuff. This is a
    new module
    
    2. User module related files are loaded by the browser only when the
    user visits the user module related areas

---
+++ app/user/user.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from'@angular/core'
+import { CommonModule } from'@angular/common'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
+import { userRoutes } from './user.routes'
+import { ProfileComponent } from './profile.component'
+
+@NgModule({
+    imports: [  
+        CommonModule,
+        RouterModule.forChild(userRoutes)
+    ],
+    declarations: [
+        ProfileComponent 
+    ],
+    providers: [
+        
+    ],
+    
+
+})
+export class UserModule{}
+
