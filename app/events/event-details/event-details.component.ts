3ba80ac72d19324b0709e4cc1fa42a137a65ac5e
Dinesh Babu <dineshbabu_kp@yahoo.com>
2017-11-08 21:44:42 +0000

    7:Using models for type safety
    1. Create IEvent and ISession models in event.model.ts
    
    2. Define the type for “event” to be Event in all the files. The will
    highlight that String is assigned where date is expected.
    
    3. Defining variables to be of model ( IEvent, ISession ) helps with
    type safety

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from '../shared/event-service'
 import { ActivatedRoute } from '@angular/router'
+import { IEvent } from '../shared/index'
 
 @Component({
     templateUrl : '/app/events/event-details/event-details.component.html',
@@ -10,7 +11,7 @@ import { ActivatedRoute } from '@angular/router'
     `]
 })
 export class EventDetailsComponent implements OnInit{
-    event: any
+    event: IEvent
 
     constructor(private eventService: EventService, private route: ActivatedRoute){
 
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event-service'
 import { ToastrService } from '../common/toastr.service'
 import { ActivatedRoute } from '@angular/router'
+import { IEvent } from './shared/index'
 
 @Component({
     template: `
